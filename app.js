0526d783bd53b0cd7e588d4b34f1b9e6c44b6ccc
Aaron <saaron@una.edu>
2023-01-24 16:02:34 -0600

    init express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000; 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World This is a working node!')
+})
+
+console.log('in the node console');
+
+app.listen(PORT, () => {
+    console.log(`Server is running & listening on port: ${PORT}`);
+  });
