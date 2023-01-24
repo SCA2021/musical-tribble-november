956479e4668c68088cad48ff2dd9bb68e0cd3858
Aaron <saaron@una.edu>
2023-01-24 16:31:19 -0600

    installed ejs

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000; 
 
+let myName = 'Sam';
 
 app.get('/', (req, res) => {
-  res.send('Hello World This is a working node!')
+  //res.send('Hello World This is a working node!')
+
+  res.send(`<h3> Hi, ${myName}</h3>`);
 })
 
 app.get('/show',(req, res) => {
